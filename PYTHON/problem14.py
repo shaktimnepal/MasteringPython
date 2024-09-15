3817aee412f0e56070c50d6125032e3898d493b5
shaktimnepal <shaktimnepal1@gmail.com>
2024-09-15 15:26:34 -0700

    Create problem14.py

---
+++ PYTHON/problem14.py
@@ -0,0 +1,17 @@
+Problem Statement:
+
+
+
+Solution:
+
+def key_with_max_value(input):
+    max_value = 0
+    for x,y in input.items():
+        if y > max_value:
+            max_value = y
+            max_key = x
+    return max_key
+
+input = {'a': 10, 'b': 20, 'c': 5}
+output = key_with_max_value(input)
+print(output)
