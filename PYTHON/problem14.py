959e261e59b31b5ad1406b5b8a68d38594e5c7d5
shaktimnepal <shaktimnepal1@gmail.com>
2024-09-15 15:30:56 -0700

    Update problem14.py

--- PYTHON/problem14.py
+++ PYTHON/problem14.py
@@ -1,6 +1,7 @@
 Problem Statement:
 
-
+Find Key with Maximum Value
+Write a Python function to find the key with the maximum value in a dictionary.
 
 Solution:
 
@@ -15,3 +16,6 @@ def key_with_max_value(input):
 input = {'a': 10, 'b': 20, 'c': 5}
 output = key_with_max_value(input)
 print(output)
+
+Sample Input: {'a': 10, 'b': 20, 'c': 5}
+Expected Output: 'b'
